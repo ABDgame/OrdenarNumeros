686b12652fa445c0f7429e3b905484ffeb52a0aa
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-10-15 14:27:10 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+let matrix = [
+  ['1', '2', '3'],
+  ['4', '5', '6'],
+  ['7', '8', ''],
+]
+
+let board = document.querySelector('.board');
+
+function drawTokens(){
+  matrix.forEach(row => row.forEach(element
+  => {
+    board.innerHTML += `<div class='token'>$
+    {element}</div>`
+  }))
+}
+
+drawTokens()
+
+
 
