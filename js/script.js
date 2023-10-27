edff94808e7ed7d5b2af0de36f25bccd88e24488
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-10-27 20:32:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-//let matrix = shuffleMatrix()
-  let matrix = [
-    ['1','2','3'],
-    ['4','5','6'],
-    ['7','','8'],
-  ]
+let matrix = shuffleMatrix()
+  //let matrix = [
+   // ['1','2','3'],
+   // ['4','5','6'],
+  // ['7','','8'],
+ // ]
 
 let board = document.querySelector('.board');
 
